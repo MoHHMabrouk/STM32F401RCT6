05ed8903a3813536faaf8b141724f3574681798b
MoHHmabrouk <mohh.mabrouk@gmail.com>
2024-05-17 16:09:49 +0300

    Some GPIO Edits

--- Src/main.c
+++ Src/main.c
@@ -5,7 +5,7 @@ int main(void) {
 	RCC_voidSysClkEn();
 	RCC_enPrephiralEnClk(AHB1_GPIOAEN);
 	GPIO_voidInit();
-	GPIO_enPinSetSpeed(PINA12, GPIO_LOW_SPEED);
+	//GPIO_enPinSetSpeed(PINA12, GPIO_LOW_SPEED);
 	GPIO_enPinSet(PINA12, HIGH);
 	while (1) {
 
